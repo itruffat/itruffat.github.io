4f1c02282d1aa0ebbbb33da449b1704797c90698
itruffat <imtruffat@gmail.com>
2025-04-17 20:20:59 +1000

    Changed update.py by putting all calls to github into a class.
    We will later be able to create an update_test.py.
    It also creates some leeway for future if we want to use bitbucket or anyhing of the sort.

--- .gitpage/update.py
+++ .gitpage/update.py
@@ -7,7 +7,7 @@ GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
 USERNAME = os.getenv("GITHUB_USERNAME")
 REPO = os.getenv("GITHUB_REPO").split("/")[1]
 
-if not GITHUB_TOKEN or not USERNAME:
+if not GITHUB_TOKEN or not USERNAME or not REPO:
     raise ValueError("""
     Missing information for request in environment variable.
     Please set it up in project configuration.
@@ -15,70 +15,71 @@ if not GITHUB_TOKEN or not USERNAME:
     Username is a varialbe, Token is a secret.
     """)
 
-HEADERS = {
+class GithubConnector:
+
+    HEADERS = {
         "Authorization": f"token {GITHUB_TOKEN}",
         "Accept": "application/vnd.github+json",
     }
 
 
-def fetch_repos():
-    repos = []
-    page = 1
-    while True:
-        url = f"https://api.github.com/user/repos?per_page=100&page={page}"
-        res = requests.get(url,headers=HEADERS)
-        data = res.json()
-        if not data:
-            break
-        repos.extend(data)
-        page += 1
-    return repos
+    def fetch_repos(self):
+        repos = []
+        page = 1
+        while True:
+            url = f"https://api.github.com/user/repos?per_page=100&page={page}"
+            res = requests.get(url,headers=GithubConnector.HEADERS)
+            data = res.json()
+            if not data:
+                break
+            repos.extend(data)
+            page += 1
+        return repos
 
-def get_file_content(owner, repo, file_path):
-    url = f"https://api.github.com/repos/{owner}/{repo}/contents/{file_path}"
-    res = requests.get(url, headers=HEADERS)
-    if res.status_code == 200:
-        content = res.json().get("content", "")
-        return base64.b64decode(content).decode('utf-8')
-    elif res.status_code == 404:
-        return None  # File does not exist
-    else:
-        raise Exception(f"Error checking {repo}: {res.status_code} {res.text}")
-
-
-    print(output_content)
+    def get_file_content(self,owner, repo, file_path):
+        url = f"https://api.github.com/repos/{owner}/{repo}/contents/{file_path}"
+        res = requests.get(url, headers=GithubConnector.HEADERS)
+        content = ""
+        if res.status_code == 200:
+            content = res.json().get("content", "")
+            return base64.b64decode(content).decode('utf-8')
+        elif res.status_code == 404:
+            return None  
+        else:
+            raise Exception(f"Error checking {repo}: {res.status_code} {res.text}")
 
 
-def get_existing_sha(owner, repo, file_path):
-    url = f"https://api.github.com/repos/{owner}/{repo}/contents/{file_path}"
-    res = requests.get(url, headers=HEADERS)
-    if res.status_code == 200:
-        return res.json().get("sha")
-    elif res.status_code == 404:
-        return None
-    else:
-        raise Exception(f"Error checking file: {res.status_code} {res.text}")
+
+    def get_existing_sha(self,owner, repo, file_path):
+        url = f"https://api.github.com/repos/{owner}/{repo}/contents/{file_path}"
+        res = requests.get(url, headers=GithubConnector.HEADERS)
+        if res.status_code == 200:
+            return res.json().get("sha")
+        elif res.status_code == 404:
+            return None
+        else:
+            raise Exception(f"Error checking file: {res.status_code} {res.text}")
 
 
-def create_or_update_file(owner, repo, file_path, new_content):
-    url = f"https://api.github.com/repos/{owner}/{repo}/contents/{file_path}"
-    encoded_content = base64.b64encode(new_content.encode()).decode()
+    def create_or_update_file(self,owner, repo, file_path, new_content):
+        url = f"https://api.github.com/repos/{owner}/{repo}/contents/{file_path}"
+        encoded_content = base64.b64encode(new_content.encode()).decode()
 
-    payload = {
-        "message": f"Automatically patched {file_path} via gitpages-update",
-        "content": encoded_content,
-        "branch": "main",  
-    }
+        payload = {
+            "message": f"Automatically patched {file_path} via gitpages-update",
+            "content": encoded_content,
+            "branch": "main",  
+        }
 
-    sha = get_existing_sha(owner, repo, file_path)
-    if sha:
-        payload["sha"] = sha  
+        sha = self.get_existing_sha(owner, repo, file_path)
+        if sha:
+            payload["sha"] = sha  
 
-    res = requests.put(url, headers=HEADERS, json=payload)
-    if res.status_code in (200, 201):
-        print(f"File '{file_path}' committed successfully!")
-    else:
-        raise Exception(f" Failed to commit: {res.status_code}\n{res.text}")
+        res = requests.put(url, headers=GithubConnector.HEADERS, json=payload)
+        if res.status_code in (200, 201):
+            print(f"File '{file_path}' committed successfully!")
+        else:
+            raise Exception(f" Failed to commit: {res.status_code}\n{res.text}")
 
 
 def produce_summary(categories, entries, summary_path):
@@ -92,8 +93,9 @@ def produce_summary(categories, entries, summary_path):
         for entry in ord_entries:
             if entries[entry]["category"] == category:
                 link = f"https://github.com/{entries[entry]['owner']}/{entry}"
-                output_content.append(f"  * [{entries[entry]['name']}]({link}):  {entries[entry]['description']}")
-
+                description = entries[entry]['description']
+                description += "\n" if description[-1] != "\n" else ""
+                output_content.append(f"  * [{entries[entry]['name']}]({link}):  {description}")
 
     old_summary = ""
     if os.path.exists(summary_path):
@@ -107,23 +109,20 @@ def produce_summary(categories, entries, summary_path):
         with open(f"{summary_path}.template", "r") as template_read:
             template_summary = template_read.read()
 
-
     new_summary = template_summary.replace("[REPOS-LIST]", new_info)
 
     return new_summary if old_summary != new_summary else None
 
 
-
-
-def main():
-    repos = fetch_repos()
+def run(connector, summary_path):
+    repos = connector.fetch_repos()
     owned = [r for r in repos if r["owner"]["login"].lower() == USERNAME.lower()]
     entries = {}
     categories = []
 
 
     for repo in owned:
-        gitpage_content = get_file_content(USERNAME, repo["name"], ".gitpage/entry.yml")
+        gitpage_content = connector.get_file_content(USERNAME, repo["name"], ".gitpage/entry.yml")
         if gitpage_content:
             print(f"Found repo: {repo['full_name']} (updated: {repo['updated_at']})")
             try:
@@ -148,18 +147,16 @@ def main():
         if content["category"] not in categories:
             categories.append(content["category"])
 
-
-    summary_path = "README.md"
-
- 
-    if(new_summary := produce_summary(categories, entries, summary_path)):
-        create_or_update_file(USERNAME,REPO, summary_path, new_summary)
+    summary = produce_summary(categories, entries, summary_path)
+    if summary:
+        connector.create_or_update_file(USERNAME,REPO, summary_path, summary)
 
 
 
 if __name__ == "__main__":
     print("started")
     print("====")
-    main()
+    connector = GithubConnector() 
+    run(connector, "README.md")
     print("====")
     print("ended")
